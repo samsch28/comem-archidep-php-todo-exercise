94bfe06e67723a0c84b90f11a3eb8bbf503428c9
Samantha Schmidt <samantha.schmidt.999@gmail.com>
2024-11-08 12:39:01 +0100

    correction de getenv

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
-define('DB_USER', getenv('todolist'));
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'));
-define('DB_NAME', getenv('todolist'));
-define('DB_HOST', getenv('127.0.0.1'));
-define('DB_PORT', getenv('3306'));
+define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
+define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
